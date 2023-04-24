386a41ee113eb5aef41a2b122646264c9bcefd56
rishig853 <rgoswami31@gatech.edu>
2023-04-23 23:54:39 -0400

    Update Constants.js

--- frontend/Constants.js
+++ frontend/Constants.js
@@ -1,2 +1,1 @@
-//export const server = "http://ec2-3-14-80-14.us-east-2.compute.amazonaws.com:3000";
-export const server = "http://localhost:3000";
+export const server = "http://ec2-3-14-80-14.us-east-2.compute.amazonaws.com:3000";
